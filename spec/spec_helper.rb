ca038070f384f7ca332500a1f572ccf7061aeaed
Daniel Infante Vargas <daninfpj@gmail.com>
2017-08-31 12:16:23 -0500

    Add code coverage metrics.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,13 @@
 require 'bundler/setup'
+require 'simplecov'
 require 'stp'
+
+if ENV['CIRCLE_ARTIFACTS']
+  dir = File.join(ENV['CIRCLE_ARTIFACTS'], "coverage")
+  SimpleCov.coverage_dir(dir)
+end
+
+SimpleCov.start
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
